8999f6368ab3da085b26d5874ab1a9b9ccc96c93
Jake <jktunney5@gmail.com>
2016-09-20 16:18:25 -0400

    resolved some api problems

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,7 +7,18 @@ import MainToolbar from '../components/Header/MainToolbar';
 import Blog from '../components/Blog/Blog';
 import MapContainerWithData from '../components/Map/Map';
 
+/*Keep some front-end principles in place
+	React works well, building app, data flows down. Events bubble up
+	Keep as few connected components as possible, often at route data
+	(high in the foodchain) adhere to the idea that ui is a function of state
+	ui should never be drawn from anything that's not state, 
+	ui is dependent variable
 
+	how do i use the info from that action to get to a new state
+	do unit testing 
+
+	see how far uni-directional data flow pattern can be used w/ graphql
+	*/
 const routes = (
 	<Route path='/' component={MainToolbar}>
 		<IndexRoute component={HomeContainer} />
--- app/data/queries/workouts.js
+++ app/data/queries/workouts.js
@@ -14,51 +14,6 @@ var MutationType = require('../types/MutationType');
 
 distance.apiKey = 'AIzaSyAJwnW0tijEPtd2YUH_e7uW1U0vIWLtg0k';
 
-function calculateDistance(params, callback){
-  distance.get(params, function(err, result){
-      var distance = result;
-      callback(null, distance)
-  });
-}
-
-	
-/*I want to go through my workout list and return 
-any workouts within 10 miles
-
-a. submit orign and destination as "params" to distance
-b. push distance to workouts object
-b. filter those workouts to return workouts greater than 10 miles*/
-
-function getWorkoutListDistances(workouts) {
-	workouts.map((workout, index)=>{
-	var lat = workout.lat;
-	var lng = workout.lng;
-	var workoutCoordinates = lat + "," + lng;
-    var params = {
-      origin: origin,
-      destination: workoutCoordinates
-    }
-
-	calculateDistance(params, (err, distance)=>{
-		if(err){
-			console.error(err)
-			}else{
-				var distanceInMeters = distance.distanceValue
-				console.log('distanceInMeters')
-				console.log(distanceInMeters)
-				console.log('workout')
-				workout.distance = distanceInMeters
-				console.log(workout)
-				return workout;
-			}		
-		}) 
-		
-	})
-};
-
-let origin = "39.292013,-76.653072";
-console.log("Hey here's getWorkoutListDistances")
-getWorkoutListDistances(WorkoutList)
 
 //Filter workouts by their distance to the origin
 function filterByDistance(obj){
@@ -74,44 +29,47 @@ function filterByDistance(obj){
 const workouts = {
   type: new List(WorkoutType),
 	resolve: (source, args) => {
-		let workoutsArray = [];
-	  	let origin = "39.292013,-76.653072";
-/*	  	console.log(WorkoutList);
-*/	  	return WorkoutList.map((workout, index)=>{
-			var lat = workout.lat;
-			var lng = workout.lng;
-			var workoutCoordinates = lat + "," + lng;
-		    var params = {
-		      origin: origin,
-		      destination: workoutCoordinates
-		    }
-			
-			/*workoutsArray.push(workout);
-			console.log("workoutsArray1");
-			console.log(workoutsArray);*/
+	/*eventually will want to turn this into a promise-based
+	architecture, callbacks ok for now*/
+	
+	//dummy variable pass
+	let origin = "39.292013,-76.653072";
+	//defining our filtered array
+	let workoutsArray = [];
+	//Using google maps package wrapper
+	function calculateDistance(params, callback){
+	  distance.get(params, function(err, result){
+	      var distance = result;
+	      callback(null, distance)
+	  });
+	}	
+	
+	function getWorkoutListDistances(workouts) {
+		workouts.map((workout, index)=>{
+		var lat = workout.lat;
+		var lng = workout.lng;
+		var workoutCoordinates = lat + "," + lng;
+	    var params = {
+	      origin: origin,
+	      destination: workoutCoordinates
+	    }
 
-			calculateDistance(params, (err, distance)=>{
-				if(err){
-					console.error(err)
-				} else {
+		calculateDistance(params, (err, distance)=>{
+			if(err){
+				console.error(err)
+				}else{
 					var distanceInMeters = distance.distanceValue
-					workout.distance = distanceInMeters
-					console.log("workout")
-					console.log(workout)
-					
-					workoutsArray.push(workout);
-					console.log("workoutsArray1");
-					console.log(workoutsArray);
-
-				};
+					workout.distance = distanceInMeters			
+				}		
 			})
-			console.log("after calculate distance");
-			return;
-	  	});
+		//Building up array of workout objects
+		workoutsArray.push(workout);
+		})
+		//returning workouts
+		return workoutsArray
+	};
 
-		console.log('workoutsArray2');
-		console.log(workoutsArray)	
-	  	return workoutsArray;
+	return getWorkoutListDistances(WorkoutList)
 	}
 };
 
