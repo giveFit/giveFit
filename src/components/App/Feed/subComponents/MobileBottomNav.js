2f2d59b294a9d446ae1eed40147416c88406c26a
Jake <jktunney5@gmail.com>
2017-03-20 02:18:50 -0400

    mobile nav

---
+++ src/components/App/Feed/subComponents/MobileBottomNav.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import FontIcon from 'material-ui/FontIcon';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+import List from 'material-ui/svg-icons/action/list';
+
+const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
+const favoritesIcon = <List />;
+const nearbyIcon = <IconLocationOn />;
+
+/**
+ * A simple example of `BottomNavigation`, with three labels and icons
+ * provided. The selected `BottomNavigationItem` is determined by application
+ * state (for instance, by the URL).
+ */
+class MobileBottomNav extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedIndex: 0,
+    };
+    this.select = this.select.bind(this);
+  }
+  
+  select(index){
+    console.log('BottomNavigation index', index)
+    this.setState({selectedIndex: index})
+  };
+
+  render() {
+    return (
+      <Paper zDepth={1}>
+        <BottomNavigation selectedIndex={this.state.selectedIndex}>
+          <BottomNavigationItem
+            label="List"
+            icon={favoritesIcon}
+            onTouchTap={() => this.select(0)}
+          />
+          <BottomNavigationItem
+            label="Map"
+            icon={nearbyIcon}
+            onTouchTap={() => this.select(1)}
+          />
+        </BottomNavigation>
+      </Paper>
+    );
+  }
+}
+
+export default MobileBottomNav;
