8c1d0271108fa1dcc267d94dac0590bf69882358
clickclickonsal <s.hernandez5400@gmail.com>
2017-05-13 00:42:27 -0400

    configure eslint rules to allow trailing commas, add eslintignore and update gitignore, add lint script to package.json

--- apolloConfig.js
+++ apolloConfig.js
@@ -1,8 +1,8 @@
 var apolloConfig = {
-	scapholdUrl: "us-west-2.api.scaphold.io/graphql/newGiveFitAlias",
-	api: "https://us-west-2.api.scaphold.io/graphql/newGiveFitAlias",
-	auth0ClientId:"hD0q9MdXUDhw3WUrcW4Zby5IMjPv0SC4",
-	auth0Domain:"jktunney.auth0.com",
-}				  
+  scapholdUrl: 'us-west-2.api.scaphold.io/graphql/newGiveFitAlias',
+  api: 'https://us-west-2.api.scaphold.io/graphql/newGiveFitAlias',
+  auth0ClientId: 'hD0q9MdXUDhw3WUrcW4Zby5IMjPv0SC4',
+  auth0Domain: 'jktunney.auth0.com',
+}
 
-module.exports = apolloConfig;
+module.exports = apolloConfig
