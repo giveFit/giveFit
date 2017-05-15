105cd13eb5bd88619a811f09731fe5f2c692faa1
Jake <jktunney5@gmail.com>
2017-05-15 16:27:38 -0400

    added snackbar for rsvp

--- src/Explore/Activities/index.js
+++ src/Explore/Activities/index.js
@@ -3,11 +3,34 @@ import PropTypes from 'prop-types'
 
 import moment from 'moment'
 
-import { CardText, Chip, Avatar } from 'material-ui'
+import { CardText, Chip, Avatar, Snackbar } from 'material-ui'
 
 import './styles.css'
 
 class Activities extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      snack: false,
+      autoHideDuration: 2000,
+      messageTrue: 'Event added to your calendar',
+      messageFalse: 'Event removed from your calendar',
+    }
+  }
+  handleRequestClose () {
+      this.setState({snack: false})
+  };
+  handleSnack () {
+    this.setState({snack: !this.state.snack})
+  }
+  handleTouchTap () {
+    window.alert('You clicked the Chip.')
+  }
+  handleActionTouchTap () {
+    this.setState({snack: false})
+    window.alert('Event removed from your calendar.')
+  };
+
   WorkoutList () {
     return this.props.workouts
       // @todo: sort this by date
@@ -40,7 +63,12 @@ class Activities extends React.Component {
               />
               <div className='__workout__information'>
                 <div>{moment(workout.startDateTime).format('ddd MMM Do, YYYY h:mm a')} - {moment(workout.endDateTime).format('LT')}</div>
-                <div><i className='fa fa-bookmark-o __share__icon' /></div>
+                <div>
+                  <i 
+                    className='fa fa-bookmark-o __share__icon' 
+                    onTouchTap={this.handleSnack.bind(this)}
+                  />
+                </div>
                 <div><i className='fa fa-share __share__icon' /></div>
               </div>
             </div>
@@ -48,6 +76,14 @@ class Activities extends React.Component {
               <div><b>Description</b></div>
               <div>{workout.description}</div>
             </div>}
+              <Snackbar
+                open={this.state.snack}
+                message={this.state.messageTrue}
+                action="undo"
+                autoHideDuration={this.state.autoHideDuration}
+                onActionTouchTap={this.handleActionTouchTap.bind(this)}
+                onRequestClose={this.handleRequestClose.bind(this)}
+              />
           </CardText>
         )
       })
