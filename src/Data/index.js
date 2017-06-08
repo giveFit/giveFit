205f0c41118de088989dadb76647b124ed407947
Jake <jktunney5@gmail.com>
2017-06-08 14:37:20 -0400

    replace google doc with gecokboard

--- src/Data/index.js
+++ src/Data/index.js
@@ -7,7 +7,7 @@ class Data extends React.Component {
     render () {
       return (
 	   <div>
-	     <iframe frameBorder="0" width="100%" height="800" src="https://docs.google.com/spreadsheets/d/1r5D19_Hh57GaXejiOQ4BP1ihLz7fauO3_XubI-wi39c/pubhtml?gid=1312350564&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+	     <iframe frameBorder="0" width="100%" height="800" src="https://share.geckoboard.com/dashboards/WQ2QZ3RODA4V2ER4"></iframe>
 	   </div>
 	 )
 	}
