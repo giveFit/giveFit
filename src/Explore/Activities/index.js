74028e4e8a95e9a3bd1a3003a45fd94b41776e49
Jake <jktunney5@gmail.com>
2017-07-09 19:26:44 -0400

    ui improvements on list

--- src/Explore/Activities/index.js
+++ src/Explore/Activities/index.js
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types'
 import moment from 'moment'
 import { graphql, compose } from 'react-apollo'
 
-import { CardText, Chip, Avatar, Snackbar } from 'material-ui'
+import { CardText, Chip, Avatar, Snackbar, RaisedButton, Paper } from 'material-ui'
+import PersonAdd from 'material-ui/svg-icons/social/person-add'
+import FitnessCenter from 'material-ui/svg-icons/places/fitness-center'
 
 import { ADD_RSVP_FOR_WORKOUT, REMOVE_RSVP_FOR_WORKOUT } from './gql.js'
 
@@ -86,70 +88,79 @@ class Activities extends React.Component {
           return null
         }
         return (
-          <CardText
-            key={`workout-${index}`}
-            className={this.props.selectedWorkoutId === workout.id ? '__selected__workout' : ''}
-          >
-            <div className='__workout__header'>
-              <Chip
-                onTouchTap={() => this.context.router.push('/profile')}
-                className='__chip'
-              >
-                <Avatar
-                  src={workout.Workout.picture}
-                  onClick={() => this.context.router.push('/profile')}
+          <Paper zDepth={5} key={`workout-${index}`}>
+            <CardText
+              className={this.props.selectedWorkoutId === workout.id ? '__selected__workout' : ''}
+            >
+              <div className='__workout__header'>
+                <Chip
+                  onTouchTap={() => this.context.router.push('/profile')}
+                  className='__chip'
+                >
+                  <Avatar
+                    src={workout.Workout.picture}
+                    onClick={() => this.context.router.push('/profile')}
+                  />
+                  <span>{workout.Workout.nickname}</span>
+                </Chip>
+                <div className='__workout__title'>
+                  <span> <b>{workout.title}</b> </span>
+                  <span>{workout.type}</span>
+                </div>
+              </div>
+              <div className='__workout__image__container'>
+                <img
+                  width='100%'
+                  onClick={() => this.props.handleWorkoutClick(workout.parkId, workout.id)}
+                  // height='300'
+                  src={workout.pictureURL}
                 />
-                <span>{workout.Workout.nickname}</span>
-              </Chip>
-              <div className='__workout__title'>
-                <span> <b>{workout.title}</b> </span>
-                <span>{workout.type}</span>
+                <div className='__workout__information'>
+                  <div>
+                    <br />
+                    <span>{this.props.indexedParks[workout.parkId].title}</span>
+                    <br />
+                    {Boolean(RSVPCount) && <span>RSVPed: {RSVPCount}</span>}
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className='__workout__image__container'>
-              <img
-                width='100%'
-                onClick={() => this.props.handleWorkoutClick(workout.parkId, workout.id)}
-                // height='300'
-                src={workout.pictureURL}
+              <div className='__description_container'>
+                <strong><span>{moment(workout.startDateTime).format('ddd MMM Do, YYYY h:mm a')} - {moment(workout.endDateTime).format('LT')}</span></strong>
+                {workout.description &&
+                  <div>{workout.description}</div>
+                }
+              </div>
+              <div>
+                {
+                  isUserRSVPed
+                    ? <RaisedButton
+                      label="You have RSVP'd!"
+                      labelPosition="before"
+                      secondary={true}
+                      icon={<FitnessCenter />}
+                      style={styles.button}
+                      onTouchTap={() => this.addRSVPForWorkout(workout.id)}
+                    />
+                    : <RaisedButton
+                      label="RSVP to this Class"
+                      labelPosition="before"
+                      primary={true}
+                      icon={<PersonAdd />}
+                      style={styles.button}
+                      onTouchTap={() => this.addRSVPForWorkout(workout.id)}
+                    />
+                }
+              </div>
+              <Snackbar
+                open={this.state.snack}
+                message={this.message}
+                action="undo"
+                autoHideDuration={this.state.autoHideDuration}
+                onActionTouchTap={this.handleActionTouchTap.bind(this)}
+                onRequestClose={this.handleRequestClose.bind(this)}
               />
-              <div className='__workout__information'>
-                <div>
-                  <span>{moment(workout.startDateTime).format('ddd MMM Do, YYYY h:mm a')} - {moment(workout.endDateTime).format('LT')}</span>
-                  <br />
-                  <span>{this.props.indexedParks[workout.parkId].title}</span>
-                  <br />
-                  {Boolean(RSVPCount) && <span>RSVPed: {RSVPCount}</span>}
-                </div>
-                <div>
-                  {
-                    isUserRSVPed
-                      ? <i
-                        className='fa fa-check-circle __share__icon'
-                        onTouchTap={() => this.removeRSVPForWorkout(workout.id)}
-                      />
-                      : <i
-                        className='fa fa-check-circle-o __share__icon'
-                        onTouchTap={() => this.addRSVPForWorkout(workout.id)}
-                      />
-                  }
-                </div>
-                <div><i className='fa fa-share __share__icon' /></div>
-              </div>
-            </div>
-            {workout.description && <div className='__description_container'>
-              <div><b>Description</b></div>
-              <div>{workout.description}</div>
-            </div>}
-            <Snackbar
-              open={this.state.snack}
-              message={this.message}
-              action="undo"
-              autoHideDuration={this.state.autoHideDuration}
-              onActionTouchTap={this.handleActionTouchTap.bind(this)}
-              onRequestClose={this.handleRequestClose.bind(this)}
-            />
-          </CardText>
+            </CardText>
+          </Paper>
         )
       })
   }
@@ -161,6 +172,12 @@ class Activities extends React.Component {
       </div>
     )
   }
+}
+
+const styles = {
+  button: {
+    margin: 12,
+  },
 }
 
 Activities.propTypes = {
